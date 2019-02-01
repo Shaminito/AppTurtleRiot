4fb76a7801e359ecac6a057a18f779c1a6e85866
Alejandro <a.c.baro@gmail.com>
2019-02-01 11:27:12 +0100

    UsuarioPojo Creado con su constructor y setters

---
+++ app/src/main/java/com/example/a21746033/appturtleriot/javaBean/UsuarioPojo.java
@@ -0,0 +1,31 @@
+package com.example.a21746033.appturtleriot.javaBean;
+
+public class UsuarioPojo {
+
+    private String user;
+    private String email;
+    private String password;
+
+    public UsuarioPojo() {
+    }
+
+    public UsuarioPojo(String user, String email, String password) {
+        this.user = user;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
