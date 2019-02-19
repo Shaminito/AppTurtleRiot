b985f3366d744a770f25b10bd13bf028bfb58e91
shaminito <XxelorketexX>
2019-02-19 18:40:05 +0100

    Nuevo atributo añadido en Playa

--- app/src/main/java/com/example/a21746033/appturtleriot/javaBean/Playa.java
+++ app/src/main/java/com/example/a21746033/appturtleriot/javaBean/Playa.java
@@ -3,7 +3,8 @@ package com.example.a21746033.appturtleriot.javaBean;
 public class Playa {
 
     private String nombre;
-    private String localizacion;
+    private String provincia;
+    private String comunidad_autonoma;
     private long latitud;
     private long altitud;
 }
