f500faea0813abe903bccda78bba529489f9709d
luiscaguana <rosnedocaguanaluis@hotmail.com>
2019-02-22 12:38:56 +0100

    hora,calendario

--- app/src/main/java/com/example/a21746033/appturtleriot/acciones/CrearAccionesFragment.java
+++ app/src/main/java/com/example/a21746033/appturtleriot/acciones/CrearAccionesFragment.java
@@ -1,6 +1,8 @@
 package com.example.a21746033.appturtleriot.acciones;
 
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
@@ -9,9 +11,12 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.a21746033.appturtleriot.AccionesFragment;
@@ -19,9 +24,15 @@ import com.example.a21746033.appturtleriot.R;
 import com.example.a21746033.appturtleriot.fbDataBase.FireDataBase;
 import com.example.a21746033.appturtleriot.javaBean.Accion;
 
+import java.util.Calendar;
+
 public class CrearAccionesFragment extends Fragment {
 
     private View v;
+    private static final String CERO = "0";
+    private static final String DOS_PUNTOS = ":";
+    private static final String BARRA = "/";
+
 
     private TextInputLayout tilTituloACC;
     private ImageView ivFotoACC;
@@ -29,7 +40,20 @@ public class CrearAccionesFragment extends Fragment {
     private EditText etFechaACC;
     private TextInputLayout tilDescripcionACC;
 
+    public final Calendar ca = Calendar.getInstance();
+    //fecha
+    final int mes = ca.get(Calendar.MONTH);
+    final int dia = ca.get(Calendar.DAY_OF_MONTH);
+    final int anio = ca.get(Calendar.YEAR);
     private ImageView ivCrearAcciones;
+    //tiempo
+    final int hora = ca.get(Calendar.HOUR_OF_DAY);
+    final int minuto = ca.get(Calendar.MINUTE);
+    //ventanas
+    EditText etFecha, etHora;
+    ImageButton ibObtenerFecha, ibObtenerHora;
+
+
 
     private RelativeLayout rlFragmentContent;
 
@@ -44,7 +68,7 @@ public class CrearAccionesFragment extends Fragment {
         tilTituloACC = v.findViewById(R.id.tilTituloACC);
         ivFotoACC = v.findViewById(R.id.ivFotoACC);
         ivPlayaACC = v.findViewById(R.id.ivPlayaACC);
-        etFechaACC = v.findViewById(R.id.etFechaACC);
+        etFechaACC = v.findViewById(R.id.et_mostrar_fecha_picker);
         tilDescripcionACC = v.findViewById(R.id.tilDescripcionACC);
 
         ivCrearAcciones = v.findViewById(R.id.ivCrearAcciones);
@@ -52,13 +76,80 @@ public class CrearAccionesFragment extends Fragment {
         rlFragmentContent = getActivity().findViewById(R.id.rlFragmentContent);
 
         fdb = new FireDataBase();
+        etFecha = v.findViewById(R.id.et_mostrar_fecha_picker);
+        etHora = v.findViewById(R.id.et_mostrar_hora_picker);
+
+        ibObtenerFecha = v.findViewById(R.id.ib_obtener_fecha);
+        ibObtenerHora = v.findViewById(R.id.ib_obtener_hora);
+
 
         c_ivFotoACC();
         c_ivPlayaACC();
         c_ivCrearAcciones();
+        ib_obtener_fecha ();
 
         return v;
     }
+
+    private void ib_obtener_fecha() {
+        ibObtenerFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recogerFecha();
+
+                {
+                }
+            }
+        });
+        ibObtenerHora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recogerHora();
+            }
+        });
+
+    }
+
+    private void recogerHora() {
+        TimePickerDialog recogerHora = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                String horaFormateada =  (hourOfDay < 9)? String.valueOf(CERO + hourOfDay) : String.valueOf(hourOfDay);
+                String minutoFormateado = (minute < 9)? String.valueOf(CERO + minute):String.valueOf(minute);
+
+                String AM_PM;
+                if(hourOfDay < 12) {
+                    AM_PM = "a.m.";
+                } else {
+                    AM_PM = "p.m.";
+                }
+
+                etHora.setText(horaFormateada + DOS_PUNTOS + minutoFormateado + " " + AM_PM);
+            }
+
+        }, hora, minuto, false);
+
+        recogerHora.show();
+
+    }
+
+    private void recogerFecha() {
+        DatePickerDialog recogerfecha = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                final int mesActual = month + 1;
+
+                String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+                String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
+
+                etFecha.setText(diaFormateado + BARRA + mesFormateado + BARRA + year);
+
+            }
+        },anio,mes,dia);
+        recogerfecha.show();
+    }
+
 
     private void c_ivFotoACC() {
         ivFotoACC.setOnClickListener(new View.OnClickListener() {
@@ -120,4 +211,6 @@ public class CrearAccionesFragment extends Fragment {
             return true;
         }
     }
+
+
 }
